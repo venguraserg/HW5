7558b9ca32e50941adab4fe4eec392da64f6b8af
Венгура Сергей <venguraserg@mail.ru>
2021-06-23 23:49:16 +0300

    Задание 1 Выполнено

--- HW5_1/Program.cs
+++ HW5_1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HW5_1
@@ -10,7 +11,7 @@ namespace HW5_1
     {
         static void Main(string[] args)
         {
-            
+
             #region Умножение матрицы на число
             //Умножение матрицы на число
             Console.Clear();
@@ -32,41 +33,57 @@ namespace HW5_1
             Console.ReadKey();
             #endregion
 
-            #region Сложение матриц
+            #region Сложение и вычитание матриц
             Console.Clear();
-            Console.WriteLine("Сложение матриц");
+            Console.WriteLine("Сложение и вычитание матриц");
             Console.WriteLine("**********************************************");
             Console.WriteLine("Инициализация матрицы А, матрица В будет иметь такую же размерность");
             int[,] matrixA = InputMatrix(InputParameter("Введите количество строк матрицы: "), InputParameter("Введите количество стролбцов матрицы: "));
-            int[,] matrixB = InputMatrix(matrixA.GetLength(0), matrixA.GetLength(1));
-            
             PrintMatrix(matrixA, "Матрица А:");
+
+            //Какойто бред конечно, но без задержки матрицы заполняются одинаково, не пойму почему,
+            //видимо Random както с системным временем завязан 
+            Thread.Sleep(10);
+
+            int[,] matrixB = InputMatrix(matrixA.GetLength(0), matrixA.GetLength(1));
             PrintMatrix(matrixB, "Матрица В:");
 
-            
+
             PrintMatrix(AddMatrix(matrixA, matrixB), "Матрица С=А+В:");
-                        
+
             PrintMatrix(SubMatrix(matrixA, matrixB), "Матрица С=А-В:");
 
             Console.WriteLine("Для продолжения нажмите любую клавишу . . . ");
             Console.ReadKey();
             #endregion
 
-            #region Вычетание матриц
+            #region Умножение матриц
+            Console.Clear();
+            Console.WriteLine("Умножение матриц");
+            Console.WriteLine("**********************************************");
+            Console.WriteLine("Инициализация матрицы А");
+            int[,] matrix_A = InputMatrix(InputParameter("Введите количество строк матрицы: "), InputParameter("Введите количество стролбцов матрицы: "));
+            PrintMatrix(matrix_A, "Матрица А:");
+
+            Console.WriteLine("Обязательное условие при умножении матриц - число столбцов матрицы А равно количеству строк матрицы B\n");
+
+            int[,] matrix_B = InputMatrix(matrix_A.GetLength(1), InputParameter("Введите количество стролбцов матрицы: "));
+            PrintMatrix(matrix_B, "Матрица B:");
+
+            PrintMatrix(MuxMatrix(matrix_A,matrix_B),"Произведение матриц А и В: ");
 
             Console.WriteLine("Для продолжения нажмите любую клавишу . . . ");
             Console.ReadKey();
             #endregion
 
-
-            Console.WriteLine("Для продолжения нажмите любую клавишу . . . ");
-            Console.ReadKey();
             
         }
         /// <summary>
-        /// Метод рандомного заполнения матрицы
+        /// Метод заполнения случайным образом
         /// </summary>
-        /// <returns>Новая рандомная матрица</returns>
+        /// <param name="row">количество строк</param>
+        /// <param name="col">количество столбцлв</param>
+        /// <returns>Возвращает двумерный массив заполненая случайным образом</returns>
         static int[,] InputMatrix(int row, int col)
         {            
             //Формируем и выводим на экран матрицу случайным образом            
@@ -83,10 +100,10 @@ namespace HW5_1
             return matrix;
         }
         /// <summary>
-        /// Метод ввода любого положительного числа
+        /// Метод ввода положительного числа
         /// </summary>
-        /// <param name="flag">Исключительно для вывода строки напоминания. true - выводим сообщение</param>
-        /// <returns>Целое положительное число</returns>
+        /// <param name="text">Текст назначения возвращаемой переменной</param>
+        /// <returns>значение типа int</returns>
         static int InputParameter(string text)
         {
             Console.Write(text);
@@ -103,9 +120,10 @@ namespace HW5_1
             return number;
         }
         /// <summary>
-        /// Выводит на экран двумерную матрицу
+        /// Метод вывода матрицы на консоль
         /// </summary>
-        /// <param name="matrix">принимает в качестве аргумента матрицу</param>
+        /// <param name="matrix">Параметр принимает двумерный массив</param>
+        /// <param name="title">Параметр вывода описания массива</param>
         static void PrintMatrix(int[,] matrix, string title)
         {
             Console.WriteLine(title);
@@ -113,17 +131,17 @@ namespace HW5_1
             {
                 for (var j = 0; j < matrix.GetLength(1); j++)
                 {
-                    Console.Write($"{matrix[i, j],5}");
+                    Console.Write($"{matrix[i, j],7}");
                 }
                 Console.WriteLine();
             }
         }
         /// <summary>
-        /// Метод умножения матрицы на число
+        /// Метод уможения матрицы на число
         /// </summary>
-        /// <param name="matrix">принимает в качестве аргумента матрицу</param>
-        /// <param name="number">принимает в качестве аргумента число для умножения</param>
-        /// <returns>Новая матрица</returns>
+        /// <param name="matrix">Принимет исходную матрицу</param>
+        /// <param name="number">принимает множитель</param>
+        /// <returns>матрицу умноженную на множитель</returns>
         static int[,] MuxMatrixNumb(int[,] matrix, int number )
         {
             int[,] newMatrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
@@ -136,7 +154,12 @@ namespace HW5_1
             }
             return newMatrix;
         }
-
+        /// <summary>
+        /// Метод сложения матриц
+        /// </summary>
+        /// <param name="matrixA">принимает матрицу А</param>
+        /// <param name="matrixB">принимает матрицу В</param>
+        /// <returns>Возращает матрицу равной сумме матриц А и В</returns>
         static int[,] AddMatrix(int[,] matrixA, int[,] matrixB)
         {
             int[,] newMatrix = new int[matrixA.GetLength(0), matrixA.GetLength(1)];
@@ -149,6 +172,12 @@ namespace HW5_1
             }
             return newMatrix;
         }
+        /// <summary>
+        /// Метод вычитания матриц
+        /// </summary>
+        /// <param name="matrixA">принимает матрицу А</param>
+        /// <param name="matrixB">принимает матрицу В</param>
+        /// <returns>Возращает матрицу равной разности матриц А и В</returns>
         static int[,] SubMatrix(int[,] matrixA, int[,] matrixB)
         {
             int[,] newMatrix = new int[matrixA.GetLength(0), matrixA.GetLength(1)];
@@ -157,6 +186,36 @@ namespace HW5_1
                 for (var j = 0; j < matrixA.GetLength(1); j++)
                 {
                     newMatrix[i, j] = matrixA[i, j] - matrixB[i, j];
+                }
+            }
+            return newMatrix;
+        }
+        /// <summary>
+        /// Метод умножения матриц
+        /// </summary>
+        /// <param name="matrixA">принимает матрицу А</param>
+        /// <param name="matrixB">принимает матрицу В</param>
+        /// <returns>Возращает матрицу равной произведению матриц А и В</returns>
+        static int[,] MuxMatrix(int[,] matrixA, int[,] matrixB)
+        {
+            int[,] newMatrix = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+
+            for (var i = 0; i < matrixA.GetLength(0); i++)
+            {
+                for (var j = 0; j < matrixB.GetLength(1); j++)
+                {
+
+                    for (var k = 0; k < matrixB.GetLength(1); k++)
+                    {
+                        int temp_1, temp_2;
+                        if (k < matrixA.GetLength(1)) temp_1 = matrixA[i, k];
+                        else temp_1 = 0;
+                        if (k < matrixB.GetLength(0)) temp_2 = matrixB[k, j];
+                        else temp_2 = 0;
+
+                        newMatrix[i, j] += temp_1 * temp_2; ;
+                        
+                    }
                 }
             }
             return newMatrix;
